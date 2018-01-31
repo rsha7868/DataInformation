3c1cdc9857a21a97628208d22faa444655f80479
Sharp <rsha7868@740s-pl-127.local>
2018-01-31 11:34:59 -0700

    Initial commit

---
+++ DataInformation/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  DataInformation
+//
+//  Created by Sharp, Ryan on 1/31/18.
+//  Copyright © 2018 Sharp, Ryan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
