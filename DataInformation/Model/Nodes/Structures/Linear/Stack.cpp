5ff2cb582cbca9a77d8d19ad7d257bf99681b1ae
Sharp <rsha7868@740s-pl-127.local>
2018-02-22 11:58:23 -0700

    SAving the linear node.

---
+++ DataInformation/Model/Nodes/Structures/Linear/Stack.cpp
@@ -0,0 +1,9 @@
+//
+//  Stack.cpp
+//  DataInformation
+//
+//  Created by Sharp, Ryan on 2/22/18.
+//  Copyright © 2018 Sharp, Ryan. All rights reserved.
+//
+
+#include "Stack.hpp"
---
+++ DataInformation/Model/Nodes/Structures/Linear/Stack.hpp
@@ -0,0 +1,14 @@
+//
+//  Stack.hpp
+//  DataInformation
+//
+//  Created by Sharp, Ryan on 2/22/18.
+//  Copyright © 2018 Sharp, Ryan. All rights reserved.
+//
+
+#ifndef Stack_hpp
+#define Stack_hpp
+
+#include <stdio.h>
+
+#endif /* Stack_hpp */
